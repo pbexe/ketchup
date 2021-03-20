25f75baeae10e6ab0dc57328a6fd09ae8ff78943
Miles Budden <contact@milesbudden.com>
2021-03-20 00:09:37 +0000

    Finished model

--- core/models.py
+++ core/models.py
@@ -16,7 +16,11 @@ class Model1(models.Model):
 class Room(models.Model):
     start_time = models.TimeField(auto_now_add=True)
 
+    def get_time(self):
+        # Do some magic maths cba rn
+        return self.start_time
+
 class UserProfile(models.Model):
     room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
-    code = models
+    code = models.CharField(max_length=16, unique=True, blank=False)
 
