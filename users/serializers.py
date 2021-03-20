e1fcfcbbe1ed04039aababf866b53f2d5d43cdc1
Miles Budden <miles@budden.net>
2021-03-20 11:46:00 +0000

    Removed Old Folders Co-authored-by: Alexander Craggs <popey456963@users.noreply.github.com> Co-authored-by: tomemmerson <tomemmerson@users.noreply.github.com> Co-authored-by: TomKelross <TomKelross@users.noreply.github.com>

--- users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import User
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username',)
--- users/views.py
+++
@@ -1,9 +0,0 @@
-from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from rest_auth.registration.views import SocialLoginView
-
-
-class GithubLogin(SocialLoginView):
-    adapter_class = GitHubOAuth2Adapter
-    callback_url = "http://localhost:8000"
-    client_class = OAuth2Client
